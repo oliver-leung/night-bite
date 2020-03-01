d54f66be692f2a500e7ee34cfdc27598920d8aff
Attilus Leung <attilusleung@gmail.com>
2020-03-01 00:56:36 -0500

    fixed affine 2

--- core/src/edu/cornell/gdiac/nightbite/ball/BallModel.java
+++ core/src/edu/cornell/gdiac/nightbite/ball/BallModel.java
@@ -1,5 +1,6 @@
 package edu.cornell.gdiac.nightbite.ball;
 
+import com.badlogic.gdx.math.Affine2;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import edu.cornell.gdiac.nightbite.obstacle.BoxObstacle;
