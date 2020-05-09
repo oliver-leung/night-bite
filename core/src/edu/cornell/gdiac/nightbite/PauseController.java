cdb48b1e62edc1285528b2bec7d2b0cf0bd6d26c
aliciaxw <alicia.xinran.wang@gmail.com>
2020-05-09 11:49:46 -0400

    Add remaining animations to pause

--- core/src/edu/cornell/gdiac/nightbite/PauseController.java
+++ core/src/edu/cornell/gdiac/nightbite/PauseController.java
@@ -47,8 +47,17 @@ public class PauseController implements Screen, InputProcessor {
     private int[] posMenu = new int[]{0, 0};
     private int[] posWASD = new int[]{0, 0};
     private int[] posWASDSprite = new int[]{0, 0};
+    private int[] posMouse = new int[]{0, 0};
+    private int[] posEsc = new int[]{0, 0};
+    private int[] posM = new int[]{0, 0};
+    private int[] posR = new int[]{0, 0};
 
-    private int[] posMove = new int[]{0, 0};
+    private int[] posControlsText = new int[]{0, 0};
+    private int[] posMoveText = new int[]{0, 0};
+    private int[] posEscText = new int[]{0, 0};
+    private int[] posWhackText = new int[]{0, 0};
+    private int[] posMuteText = new int[]{0, 0};
+    private int[] posRestartText = new int[]{0, 0};
 
     /** Display font */
     private BitmapFont displayFont;
@@ -64,18 +73,22 @@ public class PauseController implements Screen, InputProcessor {
     private FilmStrip mouseTexture;
     private FilmStrip escTexture;
     private FilmStrip mTexture;
+    private FilmStrip rTexture;
 
 
-    /** How fast we change frames (one frame per 2 calls to update */
-    private static final float ANIMATION_SPEED = 0.5f;
+    /** How fast we change frames (one frame per 4 calls to update */
+    private static final float ANIMATION_SPEED = 0.25f;
     /** Maximum number of frames before resetting to 0 */
     private static final float MAX_FRAMES = 120f;
     /** Current animation frame */
     private float frame;
-    /** Number of frames in WASD animation */
+    /** Number of frames in animations */
     private int NUM_FRAMES_WASD;
-    /** Number of frames in WASD sprite animation */
     private int NUM_FRAMES_WASDSPRITE;
+    private int NUM_FRAMES_MOUSE;
+    private int NUM_FRAMES_ESC;
+    private int NUM_FRAMES_M;
+    private int NUM_FRAMES_R;
 
 
     public PauseController(GameCanvas canvas) {
@@ -89,7 +102,7 @@ public class PauseController implements Screen, InputProcessor {
 
         background = Assets.getTextureRegion("pause/Background.png");
         menuTexture = Assets.getTextureRegion("pause/MainMenuButton.png");
-        // pauseTexture = Assets.getTextureRegion("pause/PauseTitle.png");
+        pauseTexture = Assets.getTextureRegion("pause/PauseTitle.png");
         resumeTexture = Assets.getTextureRegion("pause/ResumeButton.png");
 
         // WARNING: MAGIC NUMBERS for frame sizes
@@ -97,6 +110,14 @@ public class PauseController implements Screen, InputProcessor {
         NUM_FRAMES_WASD = wasdTexture.getSize();
         wasdSpriteTexture = Assets.getFilmStrip("pause/WASDSprite_FS.png", 128, 128);
         NUM_FRAMES_WASDSPRITE = wasdSpriteTexture.getSize();
+        mouseTexture = Assets.getFilmStrip("pause/Mouse_FS.png", 78, 100);
+        NUM_FRAMES_MOUSE = mouseTexture.getSize();
+        escTexture = Assets.getFilmStrip("pause/esc_FS.png", 48, 53);
+        NUM_FRAMES_ESC = escTexture.getSize();
+        mTexture = Assets.getFilmStrip("pause/M_FS.png", 48, 53);
+        NUM_FRAMES_M = mTexture.getSize();
+        rTexture = Assets.getFilmStrip("pause/R_FS.png", 48, 53);
+        NUM_FRAMES_R = rTexture.getSize();
     }
 
     /** Sets this controller's screen listener */
@@ -112,8 +133,12 @@ public class PauseController implements Screen, InputProcessor {
             frame = 0f;
         }
 
-        ((FilmStrip) wasdTexture).setFrame(((int) frame) % NUM_FRAMES_WASD);
-        ((FilmStrip) wasdSpriteTexture).setFrame(((int) frame) % NUM_FRAMES_WASDSPRITE);
+        wasdTexture.setFrame(((int) frame) % NUM_FRAMES_WASD);
+        wasdSpriteTexture.setFrame(((int) frame) % NUM_FRAMES_WASDSPRITE);
+        mouseTexture.setFrame(((int) frame) % NUM_FRAMES_MOUSE);
+        escTexture.setFrame(((int) frame) % NUM_FRAMES_ESC);
+        mTexture.setFrame(((int) frame) % NUM_FRAMES_M);
+        rTexture.setFrame(((int) frame) % NUM_FRAMES_R);
     }
 
     /** Draw UI assets */
@@ -125,8 +150,9 @@ public class PauseController implements Screen, InputProcessor {
         // Draw buttons
         Color tintMenu = (pressStateMenu == 1 ? Color.GRAY: Color.WHITE);
         Color tintResume = (pressStateResume == 1 ? Color.GRAY: Color.WHITE);
-        canvas.draw(menuTexture, tintMenu, 0, 0,
-                    posMenu[0], posMenu[1], 0, scale, scale);
+        canvas.draw(pauseTexture, Color.WHITE,Assets.getTextureCenterX(pauseTexture), Assets.getTextureCenterY(pauseTexture),
+                    posTitle[0], posTitle[1], 0, scale, scale);
+        canvas.draw(menuTexture, tintMenu, 0, 0, posMenu[0], posMenu[1], 0, scale, scale);
         canvas.draw(resumeTexture, tintResume, Assets.getTextureCenterX(resumeTexture), Assets.getTextureCenterY(resumeTexture),
                     posResume[0], posResume[1], 0, scale, scale);
 
@@ -135,9 +161,22 @@ public class PauseController implements Screen, InputProcessor {
                     posWASD[0], posWASD[1], 0, scale, scale);
         canvas.draw(wasdSpriteTexture, Color.WHITE, Assets.getTextureCenterX(wasdSpriteTexture), Assets.getTextureCenterY(wasdSpriteTexture),
                     posWASDSprite[0], posWASDSprite[1], 0, scale, scale);
+        canvas.draw(mouseTexture, Color.WHITE, Assets.getTextureCenterX(mouseTexture), Assets.getTextureCenterY(mouseTexture),
+                    posMouse[0], posMouse[1], 0, scale, scale);
+        canvas.draw(escTexture, Color.WHITE, Assets.getTextureCenterX(escTexture), escTexture.getRegionHeight()-15,  // hardcode origin to almost top left
+                    posEsc[0], posEsc[1], 0, scale, scale);
+        canvas.draw(mTexture, Color.WHITE, Assets.getTextureCenterX(mTexture), mTexture.getRegionHeight()-15,
+                    posM[0], posM[1], 0, scale, scale);
+        canvas.draw(rTexture, Color.WHITE, Assets.getTextureCenterX(rTexture), rTexture.getRegionHeight()-15,
+                    posR[0], posR[1], 0, scale, scale);
 
         // Draw text
-        canvas.drawText("Move", displayFont, posMove[0], posMove[1]);
+        canvas.drawTextCentered("Controls", displayFont, posControlsText[1]);
+        canvas.drawText("Move", displayFont, posMoveText[0], posMoveText[1]);
+        canvas.drawText("Pause", displayFont, posEscText[0], posEscText[1]);
+        canvas.drawText("Whack", displayFont, posWhackText[0], posWhackText[1]);
+        canvas.drawText("Mute", displayFont, posMuteText[0], posMuteText[1]);
+        canvas.drawText("Restart Level", displayFont, posRestartText[0], posRestartText[1]);
 
         canvas.end();
     }
@@ -200,7 +239,7 @@ public class PauseController implements Screen, InputProcessor {
     }
 
     /**
-     * Sets the asset location references on resize
+     * Sets the asset location references on resize and start
      */
     @Override
     public void resize(int width, int height) {
@@ -209,23 +248,56 @@ public class PauseController implements Screen, InputProcessor {
         scale = Math.max(sx, sy);
         heightY = height;
 
+        int row1 = height * 9/16;
+        int row2 = height * 7/16;
+        int row3 = height * 5/16;
+
+        int col1 = width / 4;
+        int col2 = width / 3;
+        int col3 = width / 2;
+        int col4 = width * 19/32;
+        int col5 = width * 21/32;
+
         // Top left corner
         posMenu[0] = width / 16;
         posMenu[1] = height * 14/16;
 
-        // Bottom middle
+        // Bottom center
         posResume[0] = width / 2;
-        posResume[1] = height / 4;
+        posResume[1] = height / 8;
 
-        // Tutorial images
-        posWASD[0] = width / 4;
-        posWASD[1] = height * 3/4;
-        posWASDSprite[0] = width / 2;
-        posWASDSprite[1] = height * 3/4;
+        // Top center
+        posTitle[0] = width / 2;
+        posTitle[1] = height * 7/8;
+        posControlsText[1] = height * 7/32; // offset from center y
 
-        // Tutorial text
-        posMove[0] = width / 3;
-        posMove[1] = height * 3/4;
+        // Row 1, left to right
+        posWASD[0] = col1;
+        posWASD[1] = row1;
+        posMoveText[0] = col2;
+        posMoveText[1] = row1;
+        posWASDSprite[0] = col3;
+        posWASDSprite[1] = row1;
+        posEsc[0] = col4;
+        posEsc[1] = row1;
+        posEscText[0] = col5;
+        posEscText[1] = row1;
+
+        // Row 2, left to right
+        posMouse[0] = col1;
+        posMouse[1] = row2;
+        posWhackText[0] = col2;
+        posWhackText[1] = row2;
+        posM[0] = col4;
+        posM[1] = row2;
+        posMuteText[0] = col5;
+        posMuteText[1] = row2;
+
+        // Row 3, left to right
+        posR[0] = col4;
+        posR[1] = row3;
+        posRestartText[0] = col5;
+        posRestartText[1] = row3;
     }
 
     /** Default stubs */
