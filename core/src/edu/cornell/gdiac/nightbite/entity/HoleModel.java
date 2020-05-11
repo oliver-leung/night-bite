3b28e6165507f996b4baf51e4d76293d19bb1b01
Attilus Leung <attilusleung@gmail.com>
2020-05-10 20:34:26 -0400

    happy birthday oliver

--- core/src/edu/cornell/gdiac/nightbite/entity/HoleModel.java
+++ core/src/edu/cornell/gdiac/nightbite/entity/HoleModel.java
@@ -4,4 +4,11 @@ public class HoleModel extends ImmovableModel {
     public HoleModel(float x, float y, int rotate) {
         super(x, y, rotate);
     }
+
+    // I literally cannot be bothered to do this properly
+    // This is for sorting for draw order
+    @Override
+    public float getBottom() {
+        return Float.POSITIVE_INFINITY;
+    }
 }
